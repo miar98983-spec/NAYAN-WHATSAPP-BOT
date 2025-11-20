a4bb9aad93c8e51677407fe437c93d0ba585cead
Rasel Raj <miar98983@gmail.com>
2025-11-20 21:02:23 +0600

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -12,7 +12,7 @@ module.exports = {
     permission: 2,
     prefix: true,
     category: "Moderation",
-    credit: "Developed by Mohammad Nayan",
+    credit: "Developed by Rasel Raj",
   },
 
   start: async ({ event, api, args }) => {
